c74cda0727a8d4af8b4fb422bfc132b22abc542e
VenelinZhelev <venelinzhelev7@gmail.com>
2023-03-09 14:35:31 +0200

    add RatePassword screen

--- passwords_manager/src/componens/ratePassword/RatePassword.jsx
+++ passwords_manager/src/componens/ratePassword/RatePassword.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 
 export default function RatePassword() {
 
+    function handleSubmit(event) {
+window.alert(password);
+    }
+
+      const [password, setPassword] = useState('');
+
+  const handleChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   return (
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
-
+    <Form onSubmit={(e)=>handleSubmit(e)}>
     <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
+      <Form.Label>Your Password:</Form.Label>
+      <Form.Control type="text" placeholder="type your password" onChange={(e)=>handleChange(e)} value={password} />
     </Form.Group>
     <Button variant="primary" type="submit">
-      Submit
+    Submit
     </Button>
   </Form>
   )
