de4d019654feef42d2f28239271fda7cae0a1c2a
VenelinZhelev <venelinzhelev7@gmail.com>
2023-03-10 12:15:46 +0200

    Add style to RatePassword

--- passwords_manager/src/componens/ratePassword/RatePassword.jsx
+++ passwords_manager/src/componens/ratePassword/RatePassword.jsx
@@ -2,66 +2,82 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
+import './style.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 export default function RatePassword() {
+    const [password, setPassword] = useState('');
+    const [strength, setStrength] = useState({});
 
-    function handleSubmit(event) {
-      window.alert(password);
-      var strongMeter = 0;
-      if(password.match(/[a-z]/)){
-        strongMeter++;
-      }
-      if(password.match(/[A-Z]/)){
-        strongMeter++;
-      }
-      if(password.match(/[0-9]/)){
-        strongMeter++;
-      }
-      if(password.match(/[!-/]/) || password.match(/[[-`]/i) || password.match(/[{-~]/i)){
-        strongMeter++;
-      }
-      if(password.length < 8){
-        strongMeter--;
-      }else if(password.length > 16){
-        strongMeter++;
-      }
-      switch(strongMeter){
-        
-        case 0: case 1: case 2:
-          console.log("WEAKE")
-          break;
-        case 3:
-          console.log("Mid")
-          break;
-        case 4:
-          console.log("Stronge")
-          break;
-        case 5:
-          console.log("SUPER STRONG")
-          break;
-        default:
-          console.log("KABOOM")
-          break;
-      }
-      event.preventDefault();
+    function calculateStrength() {
+        console.log(password);
+        let strength = 0;
+        if (password.match(/[a-z]/))
+            strength++;
+        if (password.match(/[A-Z]/))
+            strength++;
+        if (password.match(/[0-9]/))
+            strength++;
+        if (password.match(/[!-/]/) || password.match(/[[-`]/i) || password.match(/[{-~]/i))
+            strength+=2;
+        if (password.length < 8)
+            strength--;
+        else if (password.length > 10)
+            strength++;
+
+
+        switch (strength) {
+            case 0: case 1: case 2:
+                setStrength({
+                    text: "Weak",
+                    class: "password-strength-weak",
+                })
+                break;
+            case 3:
+                setStrength({
+                    text: "Mid",
+                    class: "password-strength-mid",
+                })
+                break;
+            case 4: case 5: 
+                setStrength({
+                    text: "Strong",
+                    class: "password-strength-strong",
+                })
+                break;
+            case 6:
+                setStrength({
+                    text: "Very Strong",
+                    class: "password-strength-very-strong",
+                })
+                break;
+            default:
+                break;
+        }
     }
 
-  const [password, setPassword] = useState('');
+    const handleChange = event => {
+        setPassword(event.target.value);
+    };
 
-  const handleChange = (event) => {
-    setPassword(event.target.value);
-  };
+    const handleSubmit = () => calculateStrength(); 
 
-  return (
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Your Password:</Form.Label>
-      <Form.Control type="text" placeholder="type your password" onChange={(e)=>handleChange(e)} value={password} />
-    </Form.Group>
-    <Button variant="primary" type="button" onClick={(e)=>handleSubmit(e)}>
-    Submit
-    </Button>
-  </Form>
-  )
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Your Password:</Form.Label>
+                <Form.Control className={strength?.class} type="text" placeholder="type your password" 
+                onChange={(e) => handleChange(e)} value={password} />
+            </Form.Group>
+            {Object.keys(strength).length !== 0 ?
+                <p1>{strength.text}</p1>
+                : null
+            }
+            <Button variant="primary" type="button" onClick={(e) => handleSubmit(e)}>
+                Submit
+            </Button>
+        </Form>
+    );
+
 }
